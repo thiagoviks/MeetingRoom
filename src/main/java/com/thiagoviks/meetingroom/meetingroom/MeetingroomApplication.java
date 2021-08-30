e29e969fedb90c954ac05a9f2d4788d1bd2e617e
thiagoviks <thiagosudgomes@gmail.com>
2021-08-30 13:21:04 -0300

    First commit with project structure

---
+++ src/main/java/com/thiagoviks/meetingroom/meetingroom/MeetingroomApplication.java
@@ -0,0 +1,13 @@
+package com.thiagoviks.meetingroom.meetingroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingroomApplication.class, args);
+	}
+
+}
